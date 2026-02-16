fd394c8c1b8b778bc23216eea175162431d7c999
ParnetsTesting <parnetstech18@gmail.com>
2026-02-16 16:37:52 +0530

    changes

--- run-create-fees-data.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Creating sample fees data...
-cd Taekwondo_backend
-node create-sample-fees-data.js
-pause
--- update-admission-schema.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Updating Admission Schema...
-node scripts/update-admission-schema.js
-pause
